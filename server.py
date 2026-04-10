5faa14739c05edd503841e54a9783158cdb4b0c2
mindx2638-del <mindx2638@gmail.com>
2026-04-10 15:46:48 +0600

    Fix typo in main check for server execution

--- server.py
+++ server.py
@@ -201,7 +201,7 @@ async def get_media(url: str, request: Request):
 # -----------------------------
 # RUNNER
 # -----------------------------
-if _name_ == "_main_":
+if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
